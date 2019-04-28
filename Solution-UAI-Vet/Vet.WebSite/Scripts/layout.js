6a079e91d480d5d036354ff5597d3edf5ff2097e
Joel Di Rosa <joel.dirosa@hotmail.com>
2019-04-27 21:03:27 -0300

    Commit maqueta

---
+++ Solution-UAI-Vet/Vet.WebSite/Scripts/layout.js
@@ -0,0 +1,15 @@
+﻿$(window).scroll(function () {
+    if ($(window).scrollTop() >= 100) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
+
+
+$('.up-to-top').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 200);
+})
