dd331aac320fb1e34f7cb1548125b501e4fa3b19
J Dirosa <jdirosa@Hexacta.onmicrosoft.com>
2019-06-29 22:53:08 -0300

    Commit Scaffolding templates t4

---
+++ Solution-UAI-Vet/Vet.WebSite/Controllers/AppointmentController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientPatientManagement.Core.Model;
+
+namespace WebApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private VetDbContext db = new VetDbContext();
+
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            
+            var appointments = db.Appointments.Include(a => a.Doctor).Include(a => a.Patient).Include(a => a.Room);
+            return View(appointments.ToList());
+        }
+
+        // GET: Appointment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Include(a => a.Doctor).Include(a => a.Patient).Include(a => a.Room).Where(x => x.Id == id).Single();
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointment/Create
+        public ActionResult Create()
+        {
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Name");
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Name");
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name");
+            return View();
+        }
+
+        // POST: Appointment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Date,Hour,DoctorId,PatientId,RoomId")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Name", appointment.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Name", appointment.PatientId);
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", appointment.RoomId);
+            return View(appointment);
+        }
+
+        // GET: Appointment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Name", appointment.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Name", appointment.PatientId);
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", appointment.RoomId);
+            return View(appointment);
+        }
+
+        // POST: Appointment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Date,Hour,DoctorId,PatientId,RoomId")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Name", appointment.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Name", appointment.PatientId);
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", appointment.RoomId);
+            return View(appointment);
+        }
+
+        // GET: Appointment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Solution-UAI-Vet/Vet.WebSite/Controllers/PatientController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientPatientManagement.Core.Data;
+using ClientPatientManagement.Core.Model;
+
+namespace WebApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        private VetDbContext db = new VetDbContext();
+
+        // GET: Patient
+        public ActionResult Index()
+        {
+            var patients = db.Patients.Include(p => p.Owner);
+            return View(patients.ToList());
+        }
+
+        // GET: Patient/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patient/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name");
+            return View();
+        }
+
+        // POST: Patient/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ClientId,Name,Gender")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", patient.ClientId);
+            return View(patient);
+        }
+
+        // GET: Patient/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", patient.ClientId);
+            return View(patient);
+        }
+
+        // POST: Patient/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ClientId,Name,Gender")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", patient.ClientId);
+            return View(patient);
+        }
+
+        // GET: Patient/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patient/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
