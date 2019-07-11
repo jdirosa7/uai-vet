b08a909b652fab025be0e24b13e08223f6320b2c
J Dirosa <jdirosa@Hexacta.onmicrosoft.com>
2019-07-11 15:52:44 -0300

    Commit fix layout & look & feel

--- Solution-UAI-Vet/Vet.WebSite/Scripts/layout.js
+++ Solution-UAI-Vet/Vet.WebSite/Scripts/layout.js
@@ -1,12 +1,22 @@
 ﻿$(window).scroll(function () {
-    if ($(window).scrollTop() >= 100) {
-        $('nav').addClass('fixed-header');
-        $('nav div').addClass('visible-title');
+
+    var menu = document.getElementById("menu");
+    var sticky = menu.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        menu.classList.add("sticky");
+    } else {
+        menu.classList.remove("sticky");
     }
-    else {
-        $('nav').removeClass('fixed-header');
-        $('nav div').removeClass('visible-title');
-    }
+
+    //if ($(window).scrollTop() >= 100) {
+    //    $('nav').addClass('fixed-header');
+    //    $('nav div').addClass('visible-title');
+    //}
+    //else {
+    //    $('nav').removeClass('fixed-header');
+    //    $('nav div').removeClass('visible-title');
+    //}
 });
 
 
