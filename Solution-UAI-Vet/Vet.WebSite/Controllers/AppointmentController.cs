01d0b4d581a9f866ca69225cf9e49052d81f81e3
J Dirosa <jdirosa@Hexacta.onmicrosoft.com>
2019-08-07 22:21:45 -0300

    Commit scheduler template

--- Solution-UAI-Vet/Vet.WebSite/Controllers/AppointmentController.cs
+++ Solution-UAI-Vet/Vet.WebSite/Controllers/AppointmentController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using ClientPatientManagement.Core.Model;
 using Vet.Business;
 using Vet.Domain;
+using Vet.WebSite.Utils;
 
 namespace WebApp.Controllers
 {
@@ -21,29 +22,74 @@ namespace WebApp.Controllers
         // GET: Appointment
         public ActionResult Index()
         {
-            var appointments = appointmentBusiness.List();
-            return View(AppointmentModel.ToModelList(appointments));
+            try
+            {
+                var appointments = appointmentBusiness.List();
+                return View(AppointmentModel.ToModelList(appointments));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
+        }
+
+        public ActionResult Calendar()
+        {
+            IEnumerable<Appointment> appointments = appointmentBusiness.List();
+
+            List<CalendarModel> calendarItems = new List<CalendarModel>();
+
+            appointments.ToList().ForEach(item =>
+            {
+                calendarItems.Add(AppointmentModel.ToCalendarModel(item));
+            });
+            
+            ViewBag.Appointments = ToJSON(calendarItems);
+            return View();
+        }
+
+        public static string ToJSON(List<CalendarModel> obj)
+        {
+            var oSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            return oSerializer.Serialize(obj);
         }
 
         // GET: Appointment/Details/5
         public ActionResult Details(int id)
         {
-            Appointment appointment = appointmentBusiness.GetById(id);
-            if (appointment == null)
+            try
             {
-                return HttpNotFound();
-            }
+                Appointment appointment = appointmentBusiness.GetById(id);
+                if (appointment == null)
+                {
+                    return HttpNotFound();
+                }
 
-            return View(AppointmentModel.ToModel(appointment));
+                return View(AppointmentModel.ToModel(appointment));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View(); 
+            }
         }
 
         // GET: Appointment/Create
         public ActionResult Create()
         {
-            ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
-            ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
-            ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
-            return View();
+            try
+            {
+                ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
+                ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
+                ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
 
         // POST: Appointment/Create
@@ -53,32 +99,75 @@ namespace WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Date,Hour,DoctorId,PatientId,RoomId")] AppointmentModel appointment)
         {
-            if (ModelState.IsValid)
+            try
             {
-                appointmentBusiness.Insert(AppointmentModel.FromModel(appointment));
+                if (appointment.Date == null)
+                {
+                    ModelState.AddModelError("Date", "Debe ingresar una fecha");
+                }
 
-                return RedirectToAction("Index");
+                if (appointment.Hour == 0)
+                {
+                    ModelState.AddModelError("Hour", "Debe ingresar una hora");
+                }
+
+
+                IEnumerable<Appointment> appointments = appointmentBusiness.GetByFilters(AppointmentModel.FromModel(appointment));
+
+                if (appointments.ToList().Count > 0)
+                {
+                    var ap = appointments.FirstOrDefault();
+
+                    if(ap.DoctorId == appointment.DoctorId)
+                    {
+                        ModelState.AddModelError("Model", "El doctor ya tiene un turno asignado para el día y hora seleccionados");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Model", "El paciente ya tiene un turno asignado en el día y horario seleccionados");
+                    }                    
+                }
+
+                if (ModelState.IsValid)
+                {
+                    appointmentBusiness.Insert(AppointmentModel.FromModel(appointment));
+
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
+                ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
+                ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
+                return View(appointment);
             }
-
-            ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
-            ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
-            ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
-            return View(appointment);
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
 
         // GET: Appointment/Edit/5
         public ActionResult Edit(int id)
-        {            
-            Appointment appointment = appointmentBusiness.GetById(id);
-            if (appointment == null)
+        {
+            try
             {
-                return HttpNotFound();
-            }
+                Appointment appointment = appointmentBusiness.GetById(id);
+                if (appointment == null)
+                {
+                    return HttpNotFound();
+                }
 
-            ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
-            ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
-            ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
-            return View(AppointmentModel.ToModel(appointment));
+                ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
+                ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
+                ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
+                return View(AppointmentModel.ToModel(appointment));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
 
         // POST: Appointment/Edit/5
@@ -88,28 +177,71 @@ namespace WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Date,Hour,DoctorId,PatientId,RoomId")] AppointmentModel appointment)
         {
-            if (ModelState.IsValid)
+            try
             {
-                appointmentBusiness.Update(AppointmentModel.FromModel(appointment));
+                if (appointment.Date == null)
+                {
+                    ModelState.AddModelError("Date", "Debe ingresar una fecha");
+                }
 
-                return RedirectToAction("Index");
+                if (appointment.Hour == 0)
+                {
+                    ModelState.AddModelError("Hour", "Debe ingresar una hora");
+                }
+
+
+                IEnumerable<Appointment> appointments = appointmentBusiness.GetByFilters(AppointmentModel.FromModel(appointment));
+
+                if (appointments.ToList().Count > 0)
+                {
+                    var ap = appointments.FirstOrDefault();
+
+                    if (ap.DoctorId == appointment.DoctorId)
+                    {
+                        ModelState.AddModelError("Model", "El doctor ya tiene un turno asignado para el día y hora seleccionados");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Model", "El paciente ya tiene un turno asignado en el día y horario seleccionados");
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    appointmentBusiness.Update(AppointmentModel.FromModel(appointment));
+
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
+                ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
+                ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
+                return View(appointment);
             }
-
-            ViewBag.DoctorId = new SelectList(doctorBusiness.List(), "Id", "Name");
-            ViewBag.PatientId = new SelectList(patientBusiness.List(), "Id", "Name");
-            ViewBag.RoomId = new SelectList(roomBusiness.List(), "Id", "Name");
-            return View(appointment);
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
 
         // GET: Appointment/Delete/5
         public ActionResult Delete(int id)
         {
-            Appointment appointment = appointmentBusiness.GetById(id);
-            if (appointment == null)
+            try
             {
-                return HttpNotFound();
+                Appointment appointment = appointmentBusiness.GetById(id);
+                if (appointment == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(AppointmentModel.ToModel(appointment));
             }
-            return View(AppointmentModel.ToModel(appointment));
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
 
         // POST: Appointment/Delete/5
@@ -117,9 +249,17 @@ namespace WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            appointmentBusiness.Delete(id);
+            try
+            {
+                appointmentBusiness.Delete(id);
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return View();
+            }
         }
     }
 }
